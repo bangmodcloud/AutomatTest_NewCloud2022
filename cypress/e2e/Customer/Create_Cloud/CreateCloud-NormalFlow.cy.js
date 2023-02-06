b33277db4e354cfe17c0f9f6da8b45c80337fd27
PPlaWara <waraporn.s@bangmod.co.th>
2023-02-06 12:05:59 +0700

    Add login

--- cypress/e2e/Customer/Create_Cloud/CreateCloud-NormalFlow.cy.js
+++ cypress/e2e/Customer/Create_Cloud/CreateCloud-NormalFlow.cy.js
@@ -1,20 +1,9 @@
 
 describe('Create Cloud Normal Flow', () => {
-    const email = "Wara@gmail.com";
-    const password = "1234567";
-
+   
     beforeEach(() => {
-
-        cy.intercept({
-            method: 'GET',
-            url: 'https://graphql-test.bangmod.cloud/oauth/oauth/token'
-        }).as("User")
-
-        cy.visit('https://web-test.bangmod.cloud/auth/login');
-        cy.get('#email').type(email);
-        cy.get('#password').type(password);
-        cy.get('.btn').click();
-    });
+        cy.login()
+    })
 
     it('Action success', () => {
 
