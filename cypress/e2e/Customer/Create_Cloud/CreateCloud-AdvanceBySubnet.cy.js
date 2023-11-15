42026878c82d6e435e56407970f89a75d18a6c07
PPlaWara <waraporn.s@bangmod.co.th>
2023-11-15 15:18:44 +0700

    [BCS-3646] add case Check the instance names

--- cypress/e2e/Customer/Create_Cloud/CreateCloud-AdvanceBySubnet.cy.js
+++ cypress/e2e/Customer/Create_Cloud/CreateCloud-AdvanceBySubnet.cy.js
@@ -1,190 +1,136 @@
 
 describe('Create Cloud Advance Flow by Subnet', () => {
     beforeEach(() => {
-        cy.login()
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+        cy.wait(500);
+        cy.login();
     })
-
     context('Create Cloud Select Subnet and Have Public IP', () => {
 
-        it('Action success', () => {
-            cy.get('#cloud-collapse').click({ force: true });
-            cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+
+        it('Usibirities (User select Attach From Private Network radio, click add icon in the Name column and select Subnet. The system display Subnet to choose from.)', () => {
+
             cy.get('[href="/cloud-server/new"]').click();
             cy.wait(500);
             // Step 1.
-            cy.get(':nth-child(4) > .distro').click();
+           
             cy.get('[type="submit"]').click();
             cy.wait(500);
             // Step 2.
             cy.get('[type="submit"]').click();
             // Step 3.
-            cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click({ force: true }); //Advance Network Setting button
+            cy.wait(500);
+            cy.contains('Advance Network Setting').click(); //Advance Network Setting button
             // Step 4.
             cy.get('#attach-private-network').check();
-            cy.get('.ant-table-row-expand-icon').click();
+            cy.get(':nth-child(1) > .ant-table-cell-with-append > .ant-table-row-expand-icon').click();
+            cy.get('.ant-table-row-level-1 > .ant-table-cell-with-append').should('be.visible')
             cy.get('.mr-2 > #subnet').check();
-            cy.get('#yes').check();
+
+
+        })
+
+        
+        it('Action success', () => {
+
+            cy.get('[href="/cloud-server/new"]').click();
+            cy.wait(500);
+            // Step 1.
+           
+            cy.get('[type="submit"]').click();
+            cy.wait(500);
+            // Step 2.
+            cy.get('[type="submit"]').click();
+            // Step 3.
+            cy.wait(500);
+            cy.contains('Advance Network Setting').click(); //Advance Network Setting button
+            // Step 4.
+            cy.get('#attach-private-network').check();
+            cy.get(':nth-child(1) > .ant-table-cell-with-append > .ant-table-row-expand-icon').click();
+            cy.get('.mr-2 > #subnet').check();
             cy.get('[type="submit"]').click();
             // Step 5.
-            cy.get('.text-center > .app_renderer_common_button__common-button-style > .btn > .fas').click();
+            cy.get(':nth-child(1) > :nth-child(3) > .text-center > .d-flex > .btn').click();
             cy.get('[data-row-key="publicIP"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').should('be.checked');
-            cy.get('[data-row-key="63dc6a71050f4a79b6878792"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').should('be.checked');
+            cy.get(':nth-child(2) > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').should('be.checked');
             cy.get('[type="submit"]').click();
             cy.wait(500);
             // Step 6.
             cy.get('[type="submit"]').click();
             // Step 7.
-            cy.get('[name="hostname"]').clear().type('Wara-Test-Instance');
-            cy.get('#instanceAmount').clear().type('2');
+            cy.get('[name="hostname"]').clear().type('cloud-subnet-public-1');
             cy.get('[type="submit"]').click();
             // Step 8.
             cy.get('#terms').check();
             cy.get('[type="submit"]').click();
-            cy.wait(700);
-
-        })
-
-        it('Usibirities (User select Attach From Private Network radio, click add icon in the Name column and select Subnet. The system display Subnet to choose from.)', () => {
-            cy.get('#cloud-collapse').click({ force: true });
-            cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-            cy.get('[href="/cloud-server/new"]').click();
-            cy.wait(500);
-            // Step 1.
-            cy.get(':nth-child(4) > .distro').click();
-            cy.get('[type="submit"]').click();
-            cy.wait(500);
-            // Step 2.
-            cy.get('[type="submit"]').click();
-            // Step 3.
-            cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click({ force: true }); //Advance Network Setting button
-            // Step 4.
-            cy.get('#attach-private-network').check();
-            cy.get('.ant-table-row-expand-icon').click();
-            cy.get('.card-body > :nth-child(3)').should('be.visible')
-            cy.get('.mr-2 > #subnet').check();
-
-
-        })
-
-        it('Usibirities (User select Yes radio on Do you want your instance to have a Public IP? card and click Next and click add icon security group required. \
-            The system display table of Associate Security Group to Subnet information and display Public IP to choose from.)', () => {
-
-            cy.get('#cloud-collapse').click({ force: true });
-            cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-            cy.get('[href="/cloud-server/new"]').click();
-            cy.wait(500);
-            // Step 1.
-            cy.get(':nth-child(4) > .distro').click();
-            cy.get('[type="submit"]').click();
-            cy.wait(500);
-            // Step 2.
-            cy.get('[type="submit"]').click();
-            // Step 3.
-            cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click({ force: true }); //Advance Network Setting button
-            // Step 4.
-            cy.get('#attach-private-network').check();
-            cy.get('.ant-table-row-expand-icon').click();
-            cy.get('.card-body > :nth-child(3)').should('be.visible')
-            cy.get('.mr-2 > #subnet').check();
-            cy.get('#yes').check();
-            cy.get('[type="submit"]').click();
-            // Step 5.
-            cy.get('.text-center > .app_renderer_common_button__common-button-style > .btn > .fas').click();
-            cy.get('[data-row-key="publicIP"] > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').should('be.checked');
-
+            cy.wait(40000);
 
         })
         
+
     })
 
     context('Create Cloud Select Subnet and Does not have Public IP', () => {
 
         it('Action success', () => {
-            cy.get('#cloud-collapse').click({ force: true });
-            cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+
             cy.get('[href="/cloud-server/new"]').click();
             cy.wait(500);
             // Step 1.
-            cy.get(':nth-child(4) > .distro').click();
+           
             cy.get('[type="submit"]').click();
             cy.wait(500);
             // Step 2.
             cy.get('[type="submit"]').click();
             // Step 3.
-            cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click({ force: true }); //Advance Network Setting button
+            cy.wait(500);
+            cy.contains('Advance Network Setting').click(); //Advance Network Setting button
             // Step 4.
+            cy.get('#require-ip-address').uncheck()
             cy.get('#attach-private-network').check();
-            cy.get('.ant-table-row-expand-icon').click();
+            cy.get(':nth-child(1) > .ant-table-cell-with-append > .ant-table-row-expand-icon').click();
             cy.get('.mr-2 > #subnet').check();
             cy.get('[type="submit"]').click();
             // Step 5.
-            cy.get('.text-center > .app_renderer_common_button__common-button-style > .btn > .fas').click();
-            cy.get('.ant-checkbox-input').should('be.checked');
+            cy.get(':nth-child(1) > :nth-child(3) > .text-center > .d-flex > .btn').click();
             cy.get('[type="submit"]').click();
             cy.wait(500);
             // Step 6.
             cy.get('[type="submit"]').click();
             // Step 7.
-            cy.get('[name="hostname"]').clear().type('Wara-Test-Instance');
-            cy.get('#instanceAmount').clear().type('2');
+            cy.get('[name="hostname"]').clear().type('cloud-subnet');
             cy.get('[type="submit"]').click();
             // Step 8.
             cy.get('#terms').check();
             cy.get('[type="submit"]').click();
-            cy.wait(700);
+            cy.wait(2000);
 
         })
 
-        it('Usibirities (User select Attach From Private Network radio, click add icon in the Name column and select Subnet. The system display Subnet to choose from.)', () => {
-            cy.get('#cloud-collapse').click({ force: true });
-            cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-            cy.get('[href="/cloud-server/new"]').click();
-            cy.wait(500);
-            // Step 1.
-            cy.get(':nth-child(4) > .distro').click();
-            cy.get('[type="submit"]').click();
-            cy.wait(500);
-            // Step 2.
-            cy.get('[type="submit"]').click();
-            // Step 3.
-            cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click({ force: true }); //Advance Network Setting button
-            // Step 4.
-            cy.get('#attach-private-network').check();
-            cy.get('.ant-table-row-expand-icon').click();
-            cy.get('.card-body > :nth-child(3)').should('be.visible')
-            cy.get('.mr-2 > #subnet').check();
-            cy.wait(700);
+        it(' Usabilities ( User go to List Floating IP page. The system display Instance in column Instance.)', () => {
 
+
+            cy.get('#network-collapse').first().click({ force: true });
+            cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
+            cy.get(':nth-child(1) > :nth-child(3) > .underline-link').should('have.text','cloud-nic-public-1')
+          
+            cy.wait(700);
         })
 
-        it('Usibirities (User select No radio on Do you want your instance to have a Public IP? card and click Next and add icon security group required. \
-            The system display table of Security Group information but not display Public IP to choose from.)', () => {
+        it(' Usabilities ( User go to Manage you Floating IP page. The system display Instance in Associated Instance card.)', () => {
 
-            cy.get('#cloud-collapse').click({ force: true });
-            cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-            cy.get('[href="/cloud-server/new"]').click();
-            cy.wait(500);
-            // Step 1.
-            cy.get(':nth-child(4) > .distro').click();
-            cy.get('[type="submit"]').click();
-            cy.wait(500);
-            // Step 2.
-            cy.get('[type="submit"]').click();
-            // Step 3.
-            cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click({ force: true }); //Advance Network Setting button
-            // Step 4.
-            cy.get('#attach-private-network').check();
-            cy.get('.ant-table-row-expand-icon').click();
-            cy.get('.card-body > :nth-child(3)').should('be.visible')
-            cy.get('.mr-2 > #subnet').check();
-            cy.get('[type="submit"]').click();
-            // Step 5.
-            cy.get('.text-center > .app_renderer_common_button__common-button-style > .btn > .fas').click();
-            cy.get('.ant-checkbox-input').should('be.checked');
 
+            cy.get('#network-collapse').first().click({ force: true });
+            cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
+            cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
+            cy.get(':nth-child(1) > div > .underline-link').should('have.text','cloud-nic-public-1')
+          
             cy.wait(700);
-
         })
+
+      
     })
 
 })
