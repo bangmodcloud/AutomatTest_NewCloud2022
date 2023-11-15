e77ee2f844134c8dc692ed3f0d9e11919be11c6f
PPlaWara <waraporn.s@bangmod.co.th>
2023-11-15 17:37:12 +0700

    [BCS-3650] Resized check BillingandCost My Pocket

--- cypress/e2e/Customer/Create_Cloud/ManagCloud-Resize.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManagCloud-Resize.cy.js
@@ -1,44 +1,18 @@
 
 describe('Manage Cloud / Resize Instance', () => {
     beforeEach(() => {
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
         cy.login()
     })
 
-    it('Action success', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
-        cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
-        cy.get('[type="submit"]').click();
-        cy.wait(500);
-        cy.get('[type="submit"]').click();
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยืนยันการปรับขนาด Cloud')
-            .click();
-        cy.wait(700);
-
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'ยืนยันคำสั่ง Soft Shutdown ?')
-            .contains('button', 'ยืนยัน Soft Shutdown ?')
-            .click();
-        cy.wait(700);
-
-
-    })
-
     it('Usibirities (User select the desired new size and click Next. The system leads to step 2. ปรับขนาด Disk.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+      
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(700);
@@ -46,12 +20,11 @@ describe('Manage Cloud / Resize Instance', () => {
     })
 
     it('Usibirities (User select ไม่ต้องการปรับขนาด SSD Disk radio and click Create and User click ยกเลิก. The system display modal “ยืนยันการปรับขนาด Cloud ?” and closed modal.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+  
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(500);
@@ -59,18 +32,18 @@ describe('Manage Cloud / Resize Instance', () => {
         cy.wait(500);
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยกเลิก')
+            .and('contain', 'Confirm Cloud Resize?')
+            .contains('button', 'Cancel')
             .click();
         cy.wait(700);
     })
 
     it('Usibirities (User click ยืนยันการปรับขนาด Cloud and click ยกเลิก. The system display modal “ยืนยันคำสั่ง Soft Shutdown ? “ and closed modal.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+   
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
@@ -79,90 +52,32 @@ describe('Manage Cloud / Resize Instance', () => {
         cy.wait(500);
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยืนยันการปรับขนาด Cloud')
+            .and('contain', 'Confirm Cloud Resize?')
+            .contains('button', 'Confirm Resize')
             .click();
         cy.wait(500);
 
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันคำสั่ง Soft Shutdown ?')
-            .contains('button', 'ยกเลิก')
+            .and('contain', 'Confirm Soft Shutdown')
+            .contains('button', 'Cancel')
             .click();
         cy.wait(700);
     })
 
-    it('Usibirities (User click ยืนยัน Soft Shutdow. User Resize succeed.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
-        cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
-        cy.get('[type="submit"]').click();
-        cy.wait(500);
-        cy.get('[type="submit"]').click();
-        cy.wait(500);
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยืนยันการปรับขนาด Cloud')
-            .click();
-        cy.wait(500);
-
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'ยืนยันคำสั่ง Soft Shutdown ?')
-            .contains('button', 'ยืนยัน Soft Shutdown ?')
-            .click();
-        cy.wait(700);
-
-    })
-
-    it('Action success (User try Resize and do want to resize the SSD Disk.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
-        cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
-        cy.get('[type="submit"]').click();
-        cy.wait(500);
-        cy.get('#resize-disk').check();
-        cy.get('[type="number"]').type('40');
-        cy.get('[type="submit"]').click();
-        cy.wait(500);
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยืนยันการปรับขนาด Cloud')
-            .click();
-        cy.wait(500);
-
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'ยืนยันคำสั่ง Soft Shutdown ?')
-            .contains('button', 'ยืนยัน Soft Shutdown ?')
-            .click();
-        cy.wait(700);
-
-    })
 
     it('Usibirities (User select ต้องการปรับขนาด SSD Disk radio. The system display field จำนวน Size เดิม , field  จำนวน Size ใหม่ที่คุณต้องการ และ alert note “กรุณาระบุตัวเลขมากกว่าค่าเดิมคือ 30”)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(500);
         cy.get('#resize-disk').check();
         cy.get(':nth-child(2) > .input-group > .form-control').should('value', '30').and('be.disabled');
-        cy.get('.callout').contains('กรุณาระบุตัวเลขมากกว่าค่าเดิมคือ 30');
+        cy.get('.callout').contains('Please enter a numbergreater than current size 30');
         cy.get('[type="number"]').type('40');
 
 
@@ -172,20 +87,20 @@ describe('Manage Cloud / Resize Instance', () => {
 
 
     it('Validation (User do not specify จำนวน Size ใหม่ที่คุณต้องการ. The system display alert massage “Please input data”)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+       
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(500);
         cy.get('#resize-disk').check();
         cy.get(':nth-child(2) > .input-group > .form-control').should('value', '30').and('be.disabled');
-        cy.get('.callout').contains('กรุณาระบุตัวเลขมากกว่าค่าเดิมคือ 30');
+        cy.get('.callout').contains('Please enter a numbergreater than current size 30');
         cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('กรุณาระบุข้อมูล')
+        cy.get('.text-danger').contains('Please input data')
 
 
         cy.wait(700);
@@ -193,21 +108,20 @@ describe('Manage Cloud / Resize Instance', () => {
     })
 
     it('Validation (User enters the number of Size less than 30. The system display alert massage “กรุณากรอกจำนวนมากกว่า 30 ขึ้นไป”)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(500);
         cy.get('#resize-disk').check();
         cy.get(':nth-child(2) > .input-group > .form-control').should('value', '30').and('be.disabled');
-        cy.get('.callout').contains('กรุณาระบุตัวเลขมากกว่าค่าเดิมคือ 30');
+        cy.get('.callout').contains('Please enter a numbergreater than current size 30');
         cy.get('[type="number"]').type('20');
         cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('กรุณากรอกจำนวนมากกว่า 30 ขึ้นไป')
+        cy.get('.text-danger').contains('Please enter a number greater than current size')
 
 
         cy.wait(700);
@@ -215,12 +129,11 @@ describe('Manage Cloud / Resize Instance', () => {
     })
 
     it('Usibirities (User specify จำนวน Size ใหม่ที่คุณต้องการ and click Create and click ยกลิก. The system display modal “ยืนยันการปรับขนาด Cloud ?”.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+      
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(500);
@@ -230,8 +143,8 @@ describe('Manage Cloud / Resize Instance', () => {
         cy.get('[type="submit"]').click();
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยกเลิก')
+            .and('contain', 'Confirm Cloud Resize?')
+            .contains('button', 'Cancel')
             .click();
 
         cy.wait(700);
@@ -239,12 +152,11 @@ describe('Manage Cloud / Resize Instance', () => {
     })
 
     it('Usibirities (User click ยืนยันการปรับขนาด Cloud and click ยกเลิก. The system display modal “ยืนยันคำสั่ง Soft Shutdown ? “ and closed modal.)', () => {
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/instance"]').first().click({ force: true });
+
         cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
-        cy.get('[data-toggle="dropdown"]').click({ force: true });
-        cy.get('a.no-hover').click();
-        //cy.get(':nth-child(2) > :nth-child(1) > .ant-radio-input').check();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
         cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
         cy.get('[type="submit"]').click();
         cy.wait(500);
@@ -254,19 +166,109 @@ describe('Manage Cloud / Resize Instance', () => {
         cy.get('[type="submit"]').click();
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันการปรับขนาด Cloud ?')
-            .contains('button', 'ยกเลิก')
+            .and('contain', 'Confirm Cloud Resize?')
+            .contains('button', 'Confirm Resize')
             .click();
 
         cy.wait(500);
 
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันคำสั่ง Soft Shutdown ?')
-            .contains('button', 'ยกเลิก')
+            .and('contain', 'Confirm Soft Shutdown ?')
+            .contains('button', 'Cancel')
             .click();
 
         cy.wait(700);
 
     })
+
+    it('Action success', () => {
+
+        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
+        cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
+        cy.get('[type="submit"]').click();
+        cy.wait(500);
+        cy.get('[type="submit"]').click();
+        cy.get('.modal-content')
+            .should('be.visible')
+            .and('contain', 'Confirm Cloud Resize?')
+            .contains('button', 'Confirm Resize?')
+            .click();
+        cy.wait(700);
+
+        cy.get('.modal-content')
+            .should('be.visible')
+            .and('contain', 'Confirm Soft Shutdown ?')
+            .contains('button', 'Confirm Soft Shutdown')
+            .click();
+        cy.wait(20000);
+
+
+    })
+
+    it('Action success (User try Resize and do want to resize the SSD Disk.)', () => {
+   
+        cy.get(':nth-child(2) > :nth-child(2) > .underline-link').click();
+        cy.wait(500);
+        cy.contains('Action').as('btn').click();
+        cy.contains('Resize').click();
+        cy.get(':nth-child(2) > .ant-table-selection-column > .ant-radio-wrapper > .ant-radio > .ant-radio-input').check();
+        cy.get('[type="submit"]').click();
+        cy.wait(500);
+        cy.get('#resize-disk').check();
+        cy.get('[type="number"]').type('40');
+        cy.get('[type="submit"]').click();
+        cy.wait(500);
+        cy.get('.modal-content')
+            .should('be.visible')
+            .and('contain', 'Confirm Cloud Resize?')
+            .contains('button', 'Confirm Resize')
+            .click();
+        cy.wait(500);
+
+        cy.get('.modal-content')
+            .should('be.visible')
+            .and('contain', 'Confirm Soft Shutdown ?')
+            .contains('button', 'Confirm Soft Shutdown')
+            .click();
+        cy.wait(20000);
+
+    })
+
+    it('Usabilities ( User go to Billing and Cost tab Billed. The system display Items to Resize Cloud.)', () => {
+
+   
+        cy.get('#billing-collapse').click({ force: true });
+        cy.get('#billing-and-cost').click({ force: true });
+        cy.wait(700);
+        cy.get(':nth-child(2) > .px-0').click();
+        cy.wait(500);
+        cy.get(':nth-child(1) > :nth-child(1) > .underline-link').should('have.text','ResizePlan-0.15to0.15-cloud-test-1')
+        
+        cy.wait(700);
+
+
+    })
+
+    it('Usabilities ( User go to My Pocket. The system display Items to Resize Cloud.)', () => {
+
+   
+        cy.get('#billing-collapse').click({ force: true });
+        cy.get('#my-pocket').click({ force: true });
+        cy.wait(700);
+        cy.get(':nth-child(2) > .px-0').click();
+        cy.wait(500);
+        cy.get(':nth-child(1) > :nth-child(5) > .underline-link').should('have.text','ResizePlan-0.15to0.15-cloud-test-1')
+        
+        cy.wait(700);
+
+
+    })
 })
+
+   
+
+
