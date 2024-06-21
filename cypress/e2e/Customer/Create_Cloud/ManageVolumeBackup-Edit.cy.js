7ac675f2d40bdfef2aa3dc2b68c79912b504af8b
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-21 10:07:46 +0700

    [BCS-4794] Manage Volume Backup :check label field

--- cypress/e2e/Customer/Create_Cloud/ManageVolumeBackup-Edit.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManageVolumeBackup-Edit.cy.js
@@ -6,16 +6,53 @@ describe('Manage Volume Backup / Edit General', () => {
         cy.login();
         cy.wait(1000)
 
-        // cy.get('.app_renderer_nt_consent_term-and-condition-modal__scroll-to-bottom-style > .d-flex > .btn').first().click();
-        // cy.get('.mx-3 > :nth-child(2) > .d-flex > .btn').first().click();
+
+    })
+
+    it('Usibirities (User go to Manage Volume Backup page. The system displayed Field labels as follows:\
+        General Information card : ID,Name,Description,Create at,Volume\
+        Specs Card : Size)', () => {
+
+        
+        cy.get('#volume-backup').click();
+        cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
+        // General Information card : 
+        cy.contains('label', 'ID').should('have.text', 'ID')
+        cy.contains('label', 'Name').should('have.text', 'Name')
+        cy.contains('label', 'Description').should('have.text', 'Description')
+        cy.contains('label', 'Create at').should('have.text', 'Create at')
+        cy.contains('label', 'Volume').should('have.text', 'Volume')
+        // Specs Card :
+        cy.contains('label', 'Size').should('have.text', 'Size')
+        cy.wait(1000);
+
+
+    })
+
+    it('Usibirities (User click Edit button. The system displayed Field labels as follows:\
+        General Information card : ID,Name,Description,Create at,Volume\
+        Specs Card : Size)', () => {
+
+        
+        cy.get('#volume-backup').click();
+        cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
+        cy.contains('Edit').click();
+        // General Information card : 
+        cy.contains('label', 'ID').should('have.text', 'ID')
+        cy.contains('label', 'Name').should('have.text', 'Name')
+        cy.contains('label', 'Description').should('have.text', 'Description')
+        cy.contains('label', 'Create at').should('have.text', 'Create at')
+        cy.contains('label', 'Volume').should('have.text', 'Volume')
+        // Specs Card :
+        cy.contains('label', 'Size').should('have.text', 'Size')
+        cy.wait(1000);
+
 
     })
 
     it('Action success', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('#cloud-server-collapse').click({ force: true });
-        cy.get('#volume-collapse').first().click({ force: true });
+        
         cy.get('#volume-backup').click();
         cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
         cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
@@ -29,9 +66,7 @@ describe('Manage Volume Backup / Edit General', () => {
 
     it('Validation (User did not specify field Name.  The system display alert messsage “ Please input data” )', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('#cloud-server-collapse').click({ force: true });
-        cy.get('#volume-collapse').first().click({ force: true });
+        
         cy.get('#volume-backup').click();
         cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
         cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
@@ -46,9 +81,7 @@ describe('Manage Volume Backup / Edit General', () => {
 
     it('Validation (User click  Cancel button.  The system close Edit. )', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('#cloud-server-collapse').click({ force: true });
-        cy.get('#volume-collapse').first().click({ force: true });
+        
         cy.get('#volume-backup').click();
         cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
         cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
@@ -56,19 +89,17 @@ describe('Manage Volume Backup / Edit General', () => {
         cy.get('.vars--1060101102 > .btn').click(); //Cancel buttonq
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันออกจากการแก้ไข')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
         cy.wait(1000);
 
 
     })
 
-    it('Validation (User click another action for which the edit was not saved.  The system display modal “ Leave Site ?” )', () => {
+    it('Validation (User click another action for which the edit was not saved.  The system display modal “Are you sure to leave information updating?” )', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('#cloud-server-collapse').click({ force: true });
-        cy.get('#volume-collapse').first().click({ force: true });
+        
         cy.get('#volume-backup').click();
         cy.get(':nth-child(1) > :nth-child(1) > .underline-link').click();
         cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
@@ -77,9 +108,9 @@ describe('Manage Volume Backup / Edit General', () => {
         cy.get('.ml-3 > .app_renderer_common_button__common-button-style > .btn').click();//Restore
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันออกจากการแก้ไข')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
         cy.wait(1000);
 
 
