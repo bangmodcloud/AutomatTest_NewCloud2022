b443e35a14605203f1efba50ef9986f3f45f1369
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-24 10:30:51 +0700

    [BCS-4800] Manage Network : check label field

--- cypress/e2e/Customer/Create_Cloud/ManageNetwork-CreateAllowedAddressPair.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManageNetwork-CreateAllowedAddressPair.cy.js
@@ -1,48 +1,66 @@
 describe(' Manage Network Interface / Create Allowed Address Pair', () => {
     beforeEach(() => {
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
         cy.login()
     })
 
-    it('Usibirities(User hover over Warning icon. The system display tooltip “Add an allowed address pair for this port. This will allow multiple MAC/IP address (range) pairs to pass through this port.” + button “Learn more“', () => {
-        cy.get('#cloud-collapse').click({ force: true });
+    it('Usibirities(User go to Manage Subnet page. The system displayed Field labels as follows:\
+        IP Address or CIDR,MAC Address (Optional)', () => {
+        
         cy.get('#network-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
-        cy.wait(700);
+        cy.wait(1000);
         cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
         cy.wait(700);
-        cy.get('.row > .app_renderer_common_button__common-button-style > .btn').invoke('removeAttr','target').click(); //Create button
-        cy.get('#popover-tooltips').trigger('mouseover')
-        cy.get('.popover-body').contains('Add an allowed address pair for this port. This will allow multiple MAC/IP address (range) pairs to pass through this port.');
-        cy.get('.row > a').contains('Learn More')
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
+        cy.contains('label', 'IP Address or CIDR').should('have.text', 'IP Address or CIDR')
+        cy.contains('label', 'MAC Address (Optional)').should('have.text', 'MAC Address (Optional)')
         cy.wait(700);
 
     })
 
-    it('Validation (User did not specify field IP Address or CIDR.The system. dispaly alert massege “กรุณาระบุข้อมูล”', () => {
-        cy.get('#cloud-collapse').click({ force: true });
+    it('Usibirities(User hover over Warning icon. The system display tooltip “Add an allowed address pair for this port. This will allow multiple MAC/IP address (range) pairs to pass through this port.” + button “Learn more“', () => {
+        
         cy.get('#network-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
-        cy.wait(700);
+        cy.wait(1000);
         cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
         cy.wait(700);
-        cy.get('.row > .app_renderer_common_button__common-button-style > .btn').invoke('removeAttr','target').click(); //Create button
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
+        cy.get('#popover-tooltips').trigger('mouseover')
+        cy.get('.ant-tooltip-inner').contains('Add an allowed address pair for this port. This will allow multiple MAC/IP address (range) pairs to pass through this port.');
+        cy.wait(700);
+
+    })
+
+    it('Validation (User did not specify field IP Address or CIDR.The system. dispaly alert massege “Please input data”', () => {
+       
+        cy.get('#network-collapse').click({ force: true });
+        cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
+        cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
+        cy.wait(1000);
+        cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
+        cy.wait(700);
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
         cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('กรุณาระบุข้อมูล');
+        cy.get('.text-danger').contains('Please input data');
         cy.wait(700);
 
     })
 
     it('Validation (User entered an Invalid  format IP address or CIDR. The system. dispaly alert massege “Invalid according to the format of IP Address or CIDR”', () => {
-        cy.get('#cloud-collapse').click({ force: true });
+        
         cy.get('#network-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
-        cy.wait(700);
+        cy.wait(1000);
         cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
         cy.wait(700);
-        cy.get('.row > .app_renderer_common_button__common-button-style > .btn').invoke('removeAttr','target').click(); //Create button
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
         cy.get('#ipv4').type('10')
         cy.get('[type="submit"]').click();
         cy.get('.text-danger').contains('Invalid according to the format of IP Address or CIDR');
@@ -51,14 +69,14 @@ describe(' Manage Network Interface / Create Allowed Address Pair', () => {
     })
 
     it('Validation (User entered an Invalid  format MAC address. The system. dispaly alert massege “Invalid according to the format of MAC address.”', () => {
-        cy.get('#cloud-collapse').click({ force: true });
+        
         cy.get('#network-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
-        cy.wait(700);
+        cy.wait(1000);
         cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
         cy.wait(700);
-        cy.get('.row > .app_renderer_common_button__common-button-style > .btn').invoke('removeAttr', 'target').click(); //Create button
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
         cy.get('#ipv4').type('10.0.0.0/24')
         cy.get('#macAddress').type('fa');
         cy.get('[type="submit"]').click();
@@ -68,20 +86,20 @@ describe(' Manage Network Interface / Create Allowed Address Pair', () => {
     })
 
     it('Usibirities (User click Cancel button. The system cloes page.', () => {
-        cy.get('#cloud-collapse').click({ force: true });
+     
         cy.get('#network-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
-        cy.wait(700);
+        cy.wait(1000);
         cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
         cy.wait(700);
-        cy.get('.row > .app_renderer_common_button__common-button-style > .btn').invoke('removeAttr', 'target').click(); //Create button
-        cy.get('small').click();
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
+        cy.contains('Cancel').click();
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันออกจากการแก้ไข')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
             .click();
         cy.wait(700);
 
@@ -92,10 +110,10 @@ describe(' Manage Network Interface / Create Allowed Address Pair', () => {
         cy.get('#network-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get(':nth-child(6) > :nth-child(1) > div > .underline-link').click(); //Link name
-        cy.wait(700);
+        cy.wait(1000);
         cy.get(':nth-child(2) > .px-0').click(); //tab Allowed Address Pair
         cy.wait(700);
-        cy.get('.row > .app_renderer_common_button__common-button-style > .btn').invoke('removeAttr','target').click(); //Create button
+        cy.contains('Create Allowed Address Pair').invoke('removeAttr', 'target').click(); //Create button
         cy.get('#ipv4').type('10.0.0.0/24')
         cy.get('#macAddress').type('fa:16:3e:b2:8f:68');
         cy.get('[type="submit"]').click();
