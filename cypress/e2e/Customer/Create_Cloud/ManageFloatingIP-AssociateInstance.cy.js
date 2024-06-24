4f9a650fd805e981300181558f6540e5404ff0b3
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-24 18:43:09 +0700

    [BCS-4802] Manage Floating IP : check label field

--- cypress/e2e/Customer/Create_Cloud/ManageFloatingIP-AssociateInstance.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManageFloatingIP-AssociateInstance.cy.js
@@ -5,38 +5,42 @@ describe('Manage Floating IP / Associate Instance', () => {
         })
         cy.login()
         cy.wait(2000)
-        // cy.get('.app_renderer_nt_consent_term-and-condition-modal__scroll-to-bottom-style > .d-flex > .btn').click();
-        // cy.get('.mx-3 > :nth-child(2) > .d-flex > .btn').click();
+
     })
 
+    it('Usabilities (User go to Manage Floating Ip page. The system displayed Field labels as follows: Select Network Interface', () => {
+
+        cy.get('#network-collapse').first().click({ force: true });
+        cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
+        cy.get(':nth-child(2) > :nth-child(1) > .underline-link').click();
+        cy.contains('Associate Instance').click();
+        cy.contains('label', 'Select Network Interface').should('have.text', 'Select Network Interface')
+
+        cy.wait(700);
+   
+    })
 
     it('Action success', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#network-collapse').first().click({ force: true });
         cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.ml-3 > .app_renderer_common_button__common-button-style > .btn').click();
+        cy.get(':nth-child(2) > :nth-child(1) > .underline-link').click();
+        cy.contains('Associate Instance').click();
         cy.get('.modal-content')
             .should('be.visible')
             .and('contain', 'Associate Instance');
         cy.get(':nth-child(1) > .ant-select > .ant-select-selector').click();
         cy.get(':nth-child(1) > .ant-select-item-option-content').click();
-        cy.get(':nth-child(2) > .ant-select > .ant-select-selector').click();
-        cy.get(':nth-child(1) > .ant-select-item-option-content').last().click();
-        cy.get(':nth-child(3) > .ant-select > .ant-select-selector').click();
-        cy.get(':nth-child(2) > .ant-select-item-option-content').last().click();
         cy.get('[type="submit"]').click();
         cy.wait(5000);
     })
 
     it(' validation (User did not select field select. The system alert message “ Please select data“)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#network-collapse').first().click({ force: true });
         cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.ml-3 > .app_renderer_common_button__common-button-style > .btn').click();
+        cy.get(':nth-child(2) > :nth-child(1) > .underline-link').click();
+        cy.contains('Associate Instance').click();
         cy.get('.modal-content')
             .should('be.visible')
             .and('contain', 'Associate Instance');
@@ -45,33 +49,16 @@ describe('Manage Floating IP / Associate Instance', () => {
         cy.wait(700);
     })
 
-    it(' Usabilities (User click ยกเลิก. The system close modal.)', () => {
+    it(' Usabilities (User click Canncel. The system close modal.)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#network-collapse').first().click({ force: true });
         cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.ml-3 > .app_renderer_common_button__common-button-style > .btn').click();
+        cy.get(':nth-child(2) > :nth-child(1) > .underline-link').click();
+        cy.contains('Associate Instance').click();
         cy.get('.modal-content')
             .should('be.visible')
             .and('contain', 'Associate Instance')
-            .contains('button', 'ยกเลิก')
-            .wait(200)
-            .click();
-        cy.wait(700);
-    })
-
-    it(' Usabilities (User go to Manage your Floating IP. The system display Associate Instance card)', () => {
-
-        cy.get('#cloud-collapse').click({ force: true });
-        cy.get('#network-collapse').first().click({ force: true });
-        cy.get('[href="/cloud-server/floating-ip"]').first().click({ force: true });
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.ml-3 > .app_renderer_common_button__common-button-style > .btn').click();
-        cy.get('.modal-content')
-            .should('be.visible')
-            .and('contain', 'Associate Instance')
-            .contains('button', 'ยกเลิก')
+            .contains('button', 'Cancel')
             .wait(200)
             .click();
         cy.wait(700);
