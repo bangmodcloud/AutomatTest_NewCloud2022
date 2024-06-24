17f00361fafcd85fcd85c753acbdb5df7a7184b2
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-24 09:17:54 +0700

    [BCS-4797] Create Subnet : check label field

--- cypress/e2e/Customer/Create_Cloud/Create-Subnet.cy.js
+++ cypress/e2e/Customer/Create_Cloud/Create-Subnet.cy.js
@@ -1,14 +1,40 @@
 describe('Create Subnet', () => {
     beforeEach(() => {
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
         cy.login()
+    })
+
+    it('Usibirities(User go to Manage Private Network page. The system displayed Field labels as follows:\
+        Private Network Information card : Name, Description (optional), IPv4 CIDR, subnet', () => {
+
+        cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
+        cy.get('#subnet').first().click({ force: true });
+        cy.contains('Create Subnet').click(); //Create button
+        cy.contains('label', 'Private Network').should('have.text', 'Private Network')
+        cy.contains('label', 'The private network already contains').should('have.text', 'The private network already contains')
+        cy.contains('label', 'subnets.').should('have.text', 'subnets.')
+        cy.contains('label', 'Name').should('have.text', 'Name')
+        cy.contains('label', 'Description (optional)').should('have.text', 'Description (optional)')
+        cy.contains('label', 'IPv4 CIDR').should('have.text', 'IPv4 CIDR')
+        cy.contains('label', 'Gateway').should('have.text', 'Gateway')
+        cy.contains('label', 'DNS Server Address 1').should('have.text', 'DNS Server Address 1')
+        cy.contains('label', 'DNS Server Address 2').should('have.text', 'DNS Server Address 2')
+        cy.contains('label', 'DNS Server Address 3 (Optional)').should('have.text', 'DNS Server Address 3 (Optional)')
+        cy.contains('label', 'DNS Server Address 4 (Optional)').should('have.text', 'DNS Server Address 4 (Optional)')
+
+
+        cy.wait(700);
+
+
     })
 
     it('Action success', () => {
 
-        cy.get('#cloud-server-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
+        cy.contains('Create Subnet').click(); //Create button
         cy.get('.ant-select-selector').click();
         cy.get(':nth-child(1) > .ant-select-item-option-content').click();
         cy.get('[name="subnets[0].name"]').clear().type('test-subnet');
@@ -23,14 +49,13 @@ describe('Create Subnet', () => {
 
     it('validation (User did not specify information. The system displays alert message “ Please input data”', () => {
 
-        cy.get('#cloud-server-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
+        cy.contains('Create Subnet').click(); //Create button
         cy.get('.ant-select-selector').click(); //Private Network
         cy.get(':nth-child(1) > .ant-select-item-option-content').click();
         cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('กรุณาระบุข้อมูล');
+        cy.get('.text-danger').contains('Please input data');
 
 
         cy.wait(700);
@@ -40,10 +65,9 @@ describe('Create Subnet', () => {
 
     it('validation (User did not select field select. The system displays alert message “ Please select data”', () => {
 
-        cy.get('#cloud-server-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
+        cy.contains('Create Subnet').click(); //Create button
         cy.get('[name="subnets[0].name"]').clear().type('test-subnet');
         cy.get('[name="subnets[0].ipv4cidr"]').type('10.0.0.1/8');
         cy.get('[type="submit"]').click();
@@ -57,10 +81,9 @@ describe('Create Subnet', () => {
 
     it('Validation (User specified an invalid IPv4 CIDR. The system displays alert message “Please input valid CIDR Notation”', () => {
 
-        cy.get('#cloud-server-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
+        cy.contains('Create Subnet').click(); //Create button
         cy.get('.ant-select-selector').click(); //Private Network
         cy.get(':nth-child(1) > .ant-select-item-option-content').click();
         cy.get('[name="subnets[0].name"]').clear().type('test-subnet');
@@ -74,37 +97,18 @@ describe('Create Subnet', () => {
 
     })
 
-    it('Validation (The specified IPv4 CIDR subnet is not in the Private Network loop. The system displays alert message “IPv4 CIDR ที่ระบุไม่ได้อยู่ในวง Private Network”', () => {
-
-        cy.get('#cloud-server-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
-        cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
-        cy.get('.ant-select-selector').click(); //Private Network
-        cy.get(':nth-child(1) > .ant-select-item-option-content').click();
-        cy.get('[name="subnets[0].name"]').clear().type('test-subnet');
-        cy.get('[name="subnets[0].ipv4cidr"]').type('192.168.0.0/16');
-        cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('IPv4 CIDR ที่ระบุไม่ได้อยู่ในวง Private Network');
-
-
-        cy.wait(700);
-
-
-    })
 
     it('Validation (The specified IPv4 CIDR subnet is not in the range. The system displays alert message “Please input valid network address in range”', () => {
 
-        cy.get('#cloud-server-collapse').click({ force: true });
         cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
         cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
+        cy.contains('Create Subnet').click(); //Create button
         cy.get('.ant-select-selector').click(); //Private Network
         cy.get(':nth-child(1) > .ant-select-item-option-content').click();
         cy.get('[name="subnets[0].name"]').clear().type('test-subnet');
         cy.get('[name="subnets[0].ipv4cidr"]').type('192.168.0.0/16');
         cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('IPv4 CIDR ที่ระบุไม่ได้อยู่ในวง Private Network');
+        cy.get('.text-danger').contains('Please input valid network address in range');
 
 
         cy.wait(700);
@@ -112,22 +116,4 @@ describe('Create Subnet', () => {
 
     })
 
-    it('Validation (The specified IPv4 CIDR subnet is not in the range. The system displays alert message “Please input valid network address in range”', () => {
-
-        cy.get('#cloud-server-collapse').click({ force: true });
-        cy.get('[href="/cloud-server/network-interface"]').first().click({ force: true });
-        cy.get('#subnet').first().click({ force: true });
-        cy.get('.ml-auto > .app_renderer_common_button__common-button-style > .btn').click(); //Create button
-        cy.get('.ant-select-selector').click(); //Private Network
-        cy.get(':nth-child(1) > .ant-select-item-option-content').click();
-        cy.get('[name="subnets[0].name"]').clear().type('test-subnet');
-        cy.get('[name="subnets[0].ipv4cidr"]').type('192.168.0.0/16');
-        cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('IPv4 CIDR ที่ระบุไม่ได้อยู่ในวง Private Network');
-
-
-        cy.wait(700);
-
-
-    })
 })
