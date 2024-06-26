88863154eedefdc50e52e8e8f1adf54fc4af4923
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-27 02:39:14 +0700

    [BCS-4812] Pool : Add casee check label field

--- cypress/e2e/Customer/Create_Cloud/ManagePool-EditMonitor.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManagePool-EditMonitor.cy.js
@@ -7,17 +7,73 @@ describe(' Edit Monitor', () => {
         cy.wait(4000)
     })
 
+    it('Usabilities (User go to tab Monitor button. The system displayed Field labels as follows:\
+        Monitor Information card : Name, Type, Max Retries Down, Delay (sec), Max Retries, Timeout (sec)\
+        HTTP Method, Expected Codes, URL Path', () => {
+
+        cy.get('#load-balance-collapse').first().click({ force: true });
+        cy.get('#load-balancer').click();
+        cy.wait(3000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
+        cy.get(':nth-child(4) > .px-0').click(); //Pool Tab
+        cy.get('.underline-link').first().click({ force: true });
+        cy.get(':nth-child(3) > .px-0').click(); // Monotor Tab
+        cy.contains('.card', 'Monitor Information').within(() => {
+            cy.contains('label', 'Name').should('have.text', 'Name')
+            cy.contains('label', 'Type').should('have.text', 'Type')
+            cy.contains('label', 'Max Retries Down').should('have.text', 'Max Retries Down')
+            cy.contains('label', 'Delay (sec)').should('have.text', 'Delay (sec)')
+            cy.contains('label', 'Max Retries').last().should('have.text', 'Max Retries')
+            cy.contains('label', 'Timeout (sec)').should('have.text', 'Timeout (sec)')
+            cy.contains('label', 'HTTP Method').should('have.text', 'HTTP Method')
+            cy.contains('label', 'Expected Codes').should('have.text', 'Expected Codes')
+            cy.contains('label', 'URL Path').should('have.text', 'URL Path')
+        })
+        cy.wait(700);
+
+
+    })
+
+    it('Usabilities (User go to tab Monitor button and click Edit button. The system displayed Field labels as follows:\
+        Monitor Information card : Name, Type, Max Retries Down, Delay (sec), Max Retries, Timeout (sec)\
+        HTTP Method, Expected Codes, URL Path', () => {
+
+        cy.get('#load-balance-collapse').first().click({ force: true });
+        cy.get('#load-balancer').click();
+        cy.wait(3000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
+        cy.get(':nth-child(4) > .px-0').click(); //Pool Tab
+        cy.get('.underline-link').first().click({ force: true });
+        cy.get(':nth-child(3) > .px-0').click(); // Monotor Tab
+        cy.contains('.card', 'Monitor Information').within(() => {
+            cy.contains('label', 'Name').should('have.text', 'Name')
+            cy.contains('label', 'Type').should('have.text', 'Type')
+            cy.contains('label', 'Max Retries Down').should('have.text', 'Max Retries Down')
+            cy.contains('label', 'Delay (sec)').should('have.text', 'Delay (sec)')
+            cy.contains('label', 'Max Retries').last().should('have.text', 'Max Retries')
+            cy.contains('label', 'Timeout (sec)').should('have.text', 'Timeout (sec)')
+            cy.contains('label', 'HTTP Method (Optional)').should('have.text', 'HTTP Method (Optional)')
+            cy.contains('label', 'Expected Codes (Optional)').should('have.text', 'Expected Codes (Optional)')
+            cy.contains('label', 'URL Path (Optional)').should('have.text', 'URL Path (Optional)')
+        })
+        cy.wait(700);
+
+
+    })
 
     it('Action success', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(3000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .px-0').click(); //Pool Tab
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
+        cy.get('.underline-link').first().click({ force: true });
         cy.get(':nth-child(3) > .px-0').click(); // Monotor Tab
-        cy.get('.vars--1060101102 > .btn').click();
+        cy.contains('Edit').click();
         cy.get('[name="name"]').clear().type('Test-EditMonitor');
         cy.get(':nth-child(3) > .ant-select > .ant-select-selector').click(); //Type
         cy.get(':nth-child(2) > .ant-select-item-option-content').click();
@@ -29,14 +85,15 @@ describe(' Edit Monitor', () => {
 
     it('Validation (User did not specify Name.  The system display alert message “Please input data”)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(3000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .px-0').click(); //Pool Tab
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
+        cy.get('.underline-link').first().click({ force: true });
         cy.get(':nth-child(3) > .px-0').click(); // Monotor Tab
-        cy.get('.vars--1060101102 > .btn').click();
+        cy.contains('Edit').click();
         cy.get('[name="name"]').clear();
         cy.get('[type="submit"]').click();
         cy.get('.text-danger').contains('Please input data');
@@ -47,14 +104,15 @@ describe(' Edit Monitor', () => {
 
     it('Usibirities (User select type “HTTP”.  The system display field HTTP Method (Optional) / Expected Codes (Optional) Prefill = 200 and URL Path (Optional) Prefill = / .)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(3000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .px-0').click(); //Pool Tab
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
+        cy.get('.underline-link').first().click({ force: true });
         cy.get(':nth-child(3) > .px-0').click(); // Monotor Tab
-        cy.get('.vars--1060101102 > .btn').click();
+        cy.contains('Edit').click();
         cy.get('[name="name"]').clear().type('Test-EditMonitor');
         cy.get(':nth-child(3) > .ant-select > .ant-select-selector').click(); //Type
         cy.get(':nth-child(1) > .ant-select-item-option-content').click();
@@ -65,23 +123,24 @@ describe(' Edit Monitor', () => {
 
     })
 
-    it('Usibirities (User click Cancel button and click Yes.  The system display modal “ยืนยันออกจากการแก้ไข” and close Edit.)', () => {
+    it('Usibirities (User click Cancel button and click Yes.  The system display modal “Are you sure to leave information updating?” and close Edit.)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(3000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .px-0').click(); //Pool Tab
-        cy.get('.ant-table-row > :nth-child(1) > a').click();
+        cy.get('.underline-link').first().click({ force: true });
         cy.get(':nth-child(3) > .px-0').click(); // Monotor Tab
-        cy.get('.vars--1060101102 > .btn').click(); //Edit
+        cy.contains('Edit').click(); //Edit
         cy.wait(700);
-        cy.get('.vars--1060101102 > .btn').last().click(); //Cancel
+        cy.contains('Cancel').click(); //Cancel
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันออกจากการแก้ไข')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
             .click()
         cy.wait(700);
 
