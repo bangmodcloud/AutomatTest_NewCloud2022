56237f1170a6fdf9196558429f8939b15dc1e0dc
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-27 00:17:42 +0700

    [BCS-4881]

--- cypress/e2e/Customer/Create_Cloud/ManageListener-EditListener.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManageListener-EditListener.cy.js
@@ -4,22 +4,100 @@ describe('Edit Listener', () => {
             return false
         })
         cy.login()
+        cy.wait(2000)
+       
+    })
+
+    it('Usabilities (User go to Manage Pool page. The system displayed Field labels as follows:\
+        Name, Description , Protocol, Port. Client Data Timeout ,\
+        TCP Inspect Timeout , Member Connect Timeout \
+        Member Data Timeout , Connection Limit, Insert Headers, X-Forwarded-For,\
+        X-Forwarded-Proto, X-Forwarded-Port', () => {
+
+        cy.get('#load-balance-collapse').first().click({ force: true });
+        cy.get('#load-balancer').click();
         cy.wait(4000)
-        cy.get('.app_renderer_nt_consent_term-and-condition-modal__scroll-to-bottom-style > .d-flex > .btn').click();
-        cy.get('.mx-3 > :nth-child(2) > .d-flex > .btn').click();
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
+        cy.get(':nth-child(4) > .nav-link').last().click({ force: true }); //Pool Tab
+        cy.get(':nth-child(3) > .px-0').click(); //Listener Tab
+        cy.get('.ant-table-row > :nth-child(1) > a').click();
+        cy.wait(2000)
+        cy.contains('label', 'Name').should('have.text', 'Name')
+        cy.contains('label', 'Description').should('have.text', 'Description')
+        cy.contains('label', 'Protocol').should('have.text', 'Protocol')
+        cy.contains('label', 'Port').should('have.text', 'Port')
+        cy.contains('label', 'Client Data Timeout').should('have.text', 'Client Data Timeout')
+        cy.contains('label', 'TCP Inspect Timeout').should('have.text', 'TCP Inspect Timeout')
+        cy.contains('label', 'Member Connect Timeout').should('have.text', 'Member Connect Timeout')
+        cy.contains('label', 'Member Data Timeout').should('have.text', 'Member Data Timeout')
+        cy.contains('label', 'Connection Limit').should('have.text', 'Connection Limit')
+        cy.contains('label', 'Insert Headers').should('have.text', 'Insert Headers')
+        cy.contains('span', 'X-Forwarded-For').should('have.text', 'X-Forwarded-For')
+        cy.contains('span', 'X-Forwarded-Proto').should('have.text', 'X-Forwarded-Proto')
+        cy.contains('span', 'X-Forwarded-Port').should('have.text', 'X-Forwarded-Port')
+
+        cy.contains('.card', 'Pool').within(() => {
+            cy.contains('label', 'Pool Name').should('have.text', 'Pool Name')
+        })
+        cy.wait(700);
+
+
+    })
+
+    it('Usabilities (User go to Manage Pool page. The system displayed Field labels as follows:\
+        Name, Description , Protocol, Port. Client Data Timeout ,\
+        TCP Inspect Timeout , Member Connect Timeout \
+        Member Data Timeout , Connection Limit, Insert Headers, X-Forwarded-For,\
+        X-Forwarded-Proto, X-Forwarded-Port', () => {
+
+        cy.get('#load-balance-collapse').first().click({ force: true });
+        cy.get('#load-balancer').click();
+        cy.wait(4000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
+        cy.get(':nth-child(4) > .nav-link').last().click({ force: true }); //Pool Tab
+        cy.get(':nth-child(3) > .px-0').click(); //Listener Tab
+        cy.get('.ant-table-row > :nth-child(1) > a').click();
+        cy.wait(2000)
+        cy.contains('.card', 'Listener Information').within(() => {
+            cy.contains('Edit').click()
+            cy.wait(500);
+        })
+        cy.contains('label', 'Name').should('have.text', 'Name')
+        cy.contains('label', 'Description (optional)').should('have.text', 'Description (optional)')
+        cy.contains('label', 'Protocol').should('have.text', 'Protocol')
+        cy.contains('label', 'Port').should('have.text', 'Port')
+        cy.contains('label', 'Client Data Timeout (Optional)').should('have.text', 'Client Data Timeout (Optional)')
+        cy.contains('label', 'TCP Inspect Timeout (Optional)').should('have.text', 'TCP Inspect Timeout (Optional)')
+        cy.contains('label', 'Member Connect Timeout (Optional)').should('have.text', 'Member Connect Timeout (Optional)')
+        cy.contains('label', 'Member Data Timeout (Optional)').should('have.text', 'Member Data Timeout (Optional)')
+        cy.contains('label', 'Connection Limit').should('have.text', 'Connection Limit')
+        cy.contains('label', 'Insert Headers').should('have.text', 'Insert Headers')
+        cy.contains('label', ' X-Forwarded-For').should('have.text', ' X-Forwarded-For')
+        cy.contains('label', ' X-Forwarded-Proto').should('have.text', ' X-Forwarded-Proto')
+        cy.contains('label', ' X-Forwarded-Port').should('have.text', ' X-Forwarded-Port')
+        cy.wait(700);
+
+
     })
 
     it('Action success', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
+
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(4000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .nav-link').last().click({ force: true }); //Pool Tab
         cy.get(':nth-child(3) > .px-0').click(); //Listener Tab
         cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.app_renderer_cloud-server_load-balancer_listener_detail_components_listener-information__listener-info-styles > .card > .card-header > :nth-child(2) > .app_renderer_common_button__common-button-style > .btn')
-            .click({ force: true }); //Edit Button
+        cy.wait(2000)
+        cy.contains('.card', 'Listener Information').within(() => {
+            cy.contains('Edit').click()
+            cy.wait(500);
+        })
 
         cy.get('[name="name"]').clear().type('Test-Edit-Listener');
         cy.get('[name="description"]').clear().type('Wara Test Edit Listener');
@@ -36,15 +114,19 @@ describe('Edit Listener', () => {
 
     it('Validation (User did not specify Name.  The system display alert message “Please input data”)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(4000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .nav-link').last().click({ force: true }); //Pool Tab
         cy.get(':nth-child(3) > .px-0').click(); //Listener Tab
         cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.app_renderer_cloud-server_load-balancer_listener_detail_components_listener-information__listener-info-styles > .card > .card-header > :nth-child(2) > .app_renderer_common_button__common-button-style > .btn')
-            .click({ force: true }); //Edit Button
+        cy.wait(2000)
+        cy.contains('.card', 'Listener Information').within(() => {
+            cy.contains('Edit').click()
+            cy.wait(500);
+        })
 
         cy.get('[name="name"]').clear();
         cy.get('[type="submit"]').click();
@@ -56,15 +138,19 @@ describe('Edit Listener', () => {
 
     it('Usibirities (The system is open for editing. Field Protocol and Port cannot be modified.', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(4000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .nav-link').last().click({ force: true }); //Pool Tab
         cy.get(':nth-child(3) > .px-0').click(); //Listener Tab
         cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.app_renderer_cloud-server_load-balancer_listener_detail_components_listener-information__listener-info-styles > .card > .card-header > :nth-child(2) > .app_renderer_common_button__common-button-style > .btn')
-            .click({ force: true }); //Edit Button
+        cy.wait(2000)
+        cy.contains('.card', 'Listener Information').within(() => {
+            cy.contains('Edit').click()
+            cy.wait(500);
+        })
 
         cy.get('#rc_select_0').should('be.disabled');
         cy.get('[name="port"]').should('be.disabled');
@@ -75,22 +161,26 @@ describe('Edit Listener', () => {
 
     it('Usiribities (User click Cancel button and click Yes.  The system display modal “ยืนยันออกจากการแก้ไข” and close Edit.)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#load-balancer').click();
-        cy.get(':nth-child(1) > :nth-child(2) > .underline-link').click();
+        cy.wait(4000)
+        cy.get(':nth-child(3) > :nth-child(2) > .underline-link').click();
+        cy.wait(5000)
         cy.get(':nth-child(4) > .nav-link').last().click({ force: true }); //Pool Tab
         cy.get(':nth-child(3) > .px-0').click(); //Listener Tab
         cy.get('.ant-table-row > :nth-child(1) > a').click();
-        cy.get('.app_renderer_cloud-server_load-balancer_listener_detail_components_listener-information__listener-info-styles > .card > .card-header > :nth-child(2) > .app_renderer_common_button__common-button-style > .btn')
-            .click({ force: true }); //Edit Button
+        cy.wait(2000)
+        cy.contains('.card', 'Listener Information').within(() => {
+            cy.contains('Edit').click()
+            cy.wait(500);
+        })
 
-        cy.get('.ml-auto > .vars--1060101102 > .btn').click();
+        cy.contains('Cancel').click();
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันออกจากการแก้ไข')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
             .click();
         cy.wait(700);
 
