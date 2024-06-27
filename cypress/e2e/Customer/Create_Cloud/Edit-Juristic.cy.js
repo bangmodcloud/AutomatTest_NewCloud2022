bd53cd17faa3c080f4fa4914668b6251b4390f6c
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-27 17:54:38 +0700

    [BCS-4816] Juristic : Add casee check label field

--- cypress/e2e/Customer/Create_Cloud/Edit-Juristic.cy.js
+++ cypress/e2e/Customer/Create_Cloud/Edit-Juristic.cy.js
@@ -7,12 +7,58 @@ describe('Edit e-tax information juristic', () => {
         cy.wait(1000)
     })
 
+    it('Usabirities (The system displayed Field labels as follows:>>"View Test Plan")', () => {
+
+        cy.get('[id="dropdown-profile "]').click({ force: true });
+        cy.get('#user-menu').click({ force: true });
+        cy.wait(300);
+        cy.contains('li','e-Tax Juristic').click(); //Juristic
+        cy.wait(300);
+
+        cy.contains('label', 'Juristic Name').should('have.text', 'Juristic Name');
+        cy.contains('label', 'Juristic ID').should('have.text', 'Juristic ID');
+        cy.contains('label', 'Branch ID').should('have.text', 'Branch ID');
+        cy.contains('label', 'Country').should('have.text', 'Country');
+        cy.contains('label', 'Postal Code').should('have.text', 'Postal Code');
+        cy.contains('label', 'Province').should('have.text', 'Province');
+        cy.contains('label', 'District / Area').should('have.text', 'District / Area');
+        cy.contains('label', 'Sub District / Sub-area').should('have.text', 'Sub District / Sub-area');
+        cy.contains('label', 'Road').should('have.text', 'Road');
+        cy.contains('label', 'Address').should('have.text', 'Address');
+        cy.wait(700);
+
+
+    })
+
+    it('Usabirities (User go to Request change personal information page.Then : The system displayed Field labels as follows:>>"View Test Plan")', () => {
+
+        cy.get('[id="dropdown-profile "]').click({ force: true });
+        cy.get('#user-menu').click({ force: true });
+        cy.wait(300);
+        cy.contains('li','e-Tax Juristic').click(); //Juristic
+        cy.wait(300);
+        cy.contains('Edit').click();
+
+        cy.contains('label', 'Juristic Name').should('have.text', 'Juristic Name');
+        cy.contains('label', 'Juristic ID').should('have.text', 'Juristic ID');
+        cy.contains('label', 'Branch ID').should('have.text', 'Branch ID');
+        cy.contains('label', 'Country').should('have.text', 'Country');
+        cy.contains('label', 'Postal Code').should('have.text', 'Postal Code');
+        cy.contains('label', 'Province').should('have.text', 'Province');
+        cy.contains('label', 'District / Area').should('have.text', 'District / Area');
+        cy.contains('label', 'Sub District / Sub-area').should('have.text', 'Sub District / Sub-area');
+        cy.contains('label', 'Road (Optional)').should('have.text', 'Road (Optional)');
+        cy.contains('label', 'Address').should('have.text', 'Address');
+        cy.wait(700);
+
+
+    })
 
     it('Usabilities (User does not enter fieldinput. The system display alert message “Please input data” .)', () => {
 
         cy.get('#billing-collapse').click({ force: true });
         cy.get('#confirm-payment-nt').first().click({ force: true });
-        cy.get('.card-body > :nth-child(3) > .mx-0 > .ml-3 > .far').click(); //Juristic
+        cy.get('.option').contains('span','Juristic').click(); //Juristic
         cy.wait(300);
         cy.contains('Edit').invoke('removeAttr','target').click();
         cy.wait(300);
@@ -32,8 +78,8 @@ describe('Edit e-tax information juristic', () => {
 
         cy.get('#billing-collapse').click({ force: true });
         cy.get('#confirm-payment-nt').first().click({ force: true });
-        cy.get('.card-body > :nth-child(3) > .mx-0 > .ml-3 > .far').click(); //Juristic
-        cy.contains('Edit').invoke('removeAttr','target').click();
+        cy.get('.option').contains('span','Juristic').click(); //Juristic
+        cy.contains('Edit').invoke('removeAttr', 'target').click();
         cy.wait(300);
         cy.contains('Edit').click();
         cy.get('#branchId').clear().type('00');
@@ -48,8 +94,8 @@ describe('Edit e-tax information juristic', () => {
 
         cy.get('#billing-collapse').click({ force: true });
         cy.get('#confirm-payment-nt').first().click({ force: true });
-        cy.get('.card-body > :nth-child(3) > .mx-0 > .ml-3 > .far').click(); //Juristic
-        cy.contains('Edit').invoke('removeAttr','target').click();
+        cy.get('.option').contains('span','Juristic').click(); //Juristic
+        cy.contains('Edit').invoke('removeAttr', 'target').click();
         cy.wait(300);
         cy.contains('Edit').click();
         cy.get('#juristicName').clear().type('Test-1');
@@ -64,19 +110,19 @@ describe('Edit e-tax information juristic', () => {
 
         cy.get('#billing-collapse').click({ force: true });
         cy.get('#confirm-payment-nt').first().click({ force: true });
-        cy.get('.card-body > :nth-child(3) > .mx-0 > .ml-3 > .far').click(); //Juristic
+        cy.get('.option').contains('span','Juristic').click(); //Juristic
         cy.wait(300);
-        cy.contains('Edit').invoke('removeAttr','target').click();
+        cy.contains('Edit').invoke('removeAttr', 'target').click();
         cy.wait(300);
         cy.contains('Edit').click();
         cy.wait(300);
         cy.contains('Cancel').click();
         cy.get('.modal-content')
-                .should('be.visible')
-                .and('contain', 'Are you sure to leave information updating?')
-                .wait(300)
-                .contains('button', 'Yes')
-                .click();
+            .should('be.visible')
+            .and('contain', 'Are you sure to leave information updating?')
+            .wait(300)
+            .contains('button', 'Yes')
+            .click();
         cy.wait(700);
 
 
