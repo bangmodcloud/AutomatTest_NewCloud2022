86ca2f824047e1ccdc1a595055f535b556527a0b
PPlaWara <waraporn.s@bangmod.co.th>
2024-06-27 11:26:00 +0700

    [BCS-4814] Manage SSL : check label field

--- cypress/e2e/Customer/Create_Cloud/ManageSSL-Edit.cy.js
+++ cypress/e2e/Customer/Create_Cloud/ManageSSL-Edit.cy.js
@@ -4,19 +4,60 @@ describe('Edit SSL Certificate', () => {
             return false
         })
         cy.login()
-        cy.wait(3000)
-        cy.get('.app_renderer_nt_consent_term-and-condition-modal__scroll-to-bottom-style > .d-flex > .btn').click();
-        cy.get('.mx-3 > :nth-child(2) > .d-flex > .btn').click();
+        cy.wait(2000)
+  
+    })
+
+    it('Usabirities (User go to manage SSL Certificate page..The system displayed Field labels as follows:\
+        General Information card : Name, Description\
+        Issued To card : Common Name (CN), Organization (O), Organizational Unit (OU)\
+        Issued By card : Common Name (CN), Organization (O), Organizational Unit (OU)\
+        Fingerprints card : SHA-256 Fingerprint, SHA-1 Fingerprint\
+        Validity Period card : Issued On ,Expires On', () => {
+
+        cy.get('#load-balance-collapse').first().click({ force: true });
+        cy.get('#ssl-certificate').click();
+        cy.get('.underline-link').first().click();
+        cy.wait(700);
+
+        cy.contains('.card', 'General Information').within(() => {
+            cy.contains('label', 'Name').should('have.text', 'Name');
+            cy.contains('label', 'Description').should('have.text', 'Description');
+        })
+
+        cy.contains('.card', 'Issued To').within(() => {
+            cy.contains('label', 'Common Name (CN)').should('include.text', 'Common Name (CN)');
+            cy.contains('label', 'Organization (O)').should('include.text', 'Organization (O)');
+            cy.contains('label', 'Organizational Unit (OU)').should('include.text', 'Organizational Unit (OU)');
+        })
+
+        cy.contains('.card', 'Issued By').within(() => {
+            cy.contains('label', 'Common Name (CN)').should('include.text', 'Common Name (CN)');
+            cy.contains('label', 'Organization (O)').should('include.text', 'Organization (O)');
+            cy.contains('label', 'Organizational Unit (OU)').should('include.text', 'Organizational Unit (OU)');
+        })
+
+        cy.contains('.card', 'Fingerprints').within(() => {
+            cy.contains('label', 'SHA-256 Fingerprint').should('include.text', 'SHA-256 Fingerprint');
+            cy.contains('label', 'SHA-1 Fingerprint').should('include.text', 'SHA-1 Fingerprint');
+        })
+
+        cy.contains('.card', 'Validity Period').within(() => {
+            cy.contains('label', 'Issued On').should('include.text', 'Issued On');
+            cy.contains('label', 'Expires On').should('include.text', 'Expires On');
+        })
+        cy.wait(700);
+
+
     })
 
     it('Action success', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#ssl-certificate').click();
-        cy.get('.underline-link').click();
+        cy.get('.underline-link').first().click();
         cy.wait(700);
-        cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
+        cy.contains('Edit').click(); //Edit button
 
         cy.get('[name="name"]').clear().type('test-EditSSL')
         cy.get('[name="description"]').clear().type('Automate testing Edit SSL');
@@ -28,12 +69,11 @@ describe('Edit SSL Certificate', () => {
 
     it('Validation (User did not specify Name.  The system display alert message “Please input data”)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#ssl-certificate').click();
-        cy.get('.underline-link').click();
+        cy.get('.underline-link').first().click();
         cy.wait(700);
-        cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
+        cy.contains('Edit').click(); //Edit button
 
         cy.get('[name="name"]').clear();
         cy.get('[name="description"]').clear();
@@ -46,40 +86,38 @@ describe('Edit SSL Certificate', () => {
 
     it('Validation (User click  Cancel button.  The system close Edit.)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#ssl-certificate').click();
-        cy.get('.underline-link').click();
+        cy.get('.underline-link').first().click();
         cy.wait(700);
-        cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
+        cy.contains('Edit').click(); //Edit button
 
         cy.get('.vars--1060101102 > .btn').click();
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'ยืนยันออกจากการแก้ไข')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
             .click();
         cy.wait(700);
 
 
     })
 
-    it('Validation (User click another action for which the edit was not saved.  The system display modal “ Leave Site ?”)', () => {
+    it('Validation (User click another action for which the edit was not saved.  The system display modal “Are you sure to leave information updating?”)', () => {
 
-        cy.get('#cloud-collapse').click({ force: true });
         cy.get('#load-balance-collapse').first().click({ force: true });
         cy.get('#ssl-certificate').click();
-        cy.get('.underline-link').click();
+        cy.get('.underline-link').first().click();
         cy.wait(700);
-        cy.get(':nth-child(2) > .app_renderer_common_button__common-button-style > .btn').click(); //Edit button
+        cy.contains('Edit').click(); //Edit button
 
-        cy.get('.mx-0 > .d-flex > .ml-auto > .app_renderer_common_button__common-button-style > .btn').click();
+        cy.contains('Delete').click();
         cy.get('.modal-content')
             .should('be.visible')
-            .and('contain', 'Leave Site ?')
+            .and('contain', 'Are you sure to leave information updating?')
             .wait(500)
-            .contains('button', 'ใช่')
+            .contains('button', 'Yes')
             .click();
         cy.wait(700);
 
