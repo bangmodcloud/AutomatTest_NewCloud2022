f7abcf911c696c45d68003ecf50beb3d59f008f1
PPlaWara <waraporn.s@bangmod.co.th>
2024-07-04 15:58:09 +0700

    [BCS-4829] Voucher code : check label field

--- cypress/e2e/Customer/Create_Cloud/Manage-VoucherCode.cy.js
+++ cypress/e2e/Customer/Create_Cloud/Manage-VoucherCode.cy.js
@@ -8,12 +8,58 @@ describe('Maange Voucher code', () => {
 
     })
 
+    it('Usabilities (The system displayed Field labels as follows: No., Subject, Detail, Document Date, Voucher Code, Value Credit\
+        Start Date, Expired Date, Create Date, Create by, Redeemed Date, Redeem by, Last Modified)', () => {
+
+        cy.get('#voucher-code').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('label', 'No.').should('have.text', 'No.');
+        cy.contains('label', 'Subject').should('have.text', 'Subject');
+        cy.contains('label', 'Detail').should('have.text', 'Detail');
+        cy.contains('label', 'Document Date').should('include.text', 'Document Date');
+        cy.contains('label', 'Voucher Code').should('include.text', 'Voucher Code');
+        cy.contains('label', 'Value Credit').should('include.text', 'Value Credit');
+        cy.contains('label', 'Start Date').should('include.text', 'Start Date');
+        cy.contains('label', 'Expired Date').should('include.text', 'Expired Date');
+        cy.contains('label', 'Create Date').should('include.text', 'Create Date');
+        cy.contains('label', 'Create by').should('include.text', 'Create by');
+        cy.contains('label', 'Redeemed Date').should('include.text', 'Redeemed Date');
+        cy.contains('label', 'Redeem by').should('have.text', 'Redeem by');
+        cy.contains('label', 'Last Modified').should('have.text', 'Last Modified');
+
+        cy.wait(700);
+
+    })
+
+    it('Usabilities (Admin click Edit button. The system displayed Field labels as follows: No., Subject, Detail, Document Date, \
+        Voucher Code, Value Credit, Start Date, Expired Date, Create Date, Create by, Redeemed Date, Redeem by, Last Modified)', () => {
+
+        cy.get('#voucher-code').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('Edit').click();
+        cy.contains('label', 'No.').should('have.text', 'No.');
+        cy.contains('label', 'Subject').should('have.text', 'Subject');
+        cy.contains('label', 'Detail').should('have.text', 'Detail');
+        cy.contains('label', 'Document Date').should('include.text', 'Document Date');
+        cy.contains('label', 'Voucher Code').should('include.text', 'Voucher Code');
+        cy.contains('label', 'Value Credit').should('include.text', 'Value Credit');
+        cy.contains('label', 'Start Date').should('include.text', 'Start Date');
+        cy.contains('label', 'Expired Date').should('include.text', 'Expired Date');
+        cy.contains('label', 'Create Date').should('include.text', 'Create Date');
+        cy.contains('label', 'Create by').should('include.text', 'Create by');
+        cy.contains('label', 'Redeemed Date').should('include.text', 'Redeemed Date');
+        cy.contains('label', 'Redeem by').should('have.text', 'Redeem by');
+        cy.contains('label', 'Last Modified').should('have.text', 'Last Modified');
+
+        cy.wait(700);
+
+    })
 
     it('Validation (Admin does not enter Textfield.  The system display alert messsage “ Please input data” )', () => {
 
         cy.get('#voucher-code').click();
-        cy.get(':nth-child(4) > :nth-child(1) > .underline-link').first().click();
-        cy.get('.justify-content-between > .app_renderer_nt_lib_button__nt-button-style > .btn').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('Edit').click();
         cy.get('#subject').clear();
         cy.get('#value').clear();
         cy.get('[type="submit"]').click();
@@ -25,8 +71,8 @@ describe('Maange Voucher code', () => {
     it('Usabilities (Admin click hyper link Create by. The system open new tab to Manage Use page)', () => {
 
         cy.get('#voucher-code').click();
-        cy.get(':nth-child(4) > :nth-child(1) > .underline-link').first().click();
-        cy.get('.justify-content-between > .app_renderer_nt_lib_button__nt-button-style > .btn').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('Edit').click();
         cy.wait(200);
         cy.get('.underline-link').invoke('removeAttr', 'target').click();
         cy.wait(700);
@@ -36,10 +82,10 @@ describe('Maange Voucher code', () => {
     it('Usabilities (Admin click Cancel button. The system diaplay modal “Are you sure to leave information updating?”)', () => {
 
         cy.get('#voucher-code').click();
-        cy.get(':nth-child(4) > :nth-child(1) > .underline-link').first().click();
-        cy.get('.justify-content-between > .app_renderer_nt_lib_button__nt-button-style > .btn').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('Edit').click();
         cy.wait(200);
-        cy.get('.vars-1875906202 > .btn').click();
+        cy.contains('Cancel').click();
         cy.get('.modal-content')
             .should('be.visible')
             .and('contain', 'Are you sure to leave information updating?')
@@ -53,8 +99,8 @@ describe('Maange Voucher code', () => {
     it('Action success', () => {
 
         cy.get('#voucher-code').click();
-        cy.get(':nth-child(4) > :nth-child(1) > .underline-link').first().click();
-        cy.get('.justify-content-between > .app_renderer_nt_lib_button__nt-button-style > .btn').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('Edit').click();
         cy.get('#subject').clear().type('HNY');
         cy.get('#value').clear().type('100');
         cy.get('[type="submit"]').click();
