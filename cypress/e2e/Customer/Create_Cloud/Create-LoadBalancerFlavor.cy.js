6e4be7b7e62e4578c80d582fbeddf9fa579e51e4
PPlaWara <waraporn.s@bangmod.co.th>
2024-07-08 16:59:00 +0700

    [BCS-4845] Load Balance Flavor : check label field

--- cypress/e2e/Customer/Create_Cloud/Create-LoadBalancerFlavor.cy.js
+++ cypress/e2e/Customer/Create_Cloud/Create-LoadBalancerFlavor.cy.js
@@ -7,39 +7,45 @@ describe('Create Load Balancer Flavor', () => {
 
     })
 
-    it('Validation (Admin did not specify information.  The system display alert messsage “ Please input data” )', () => {
+    it('Usibirities (The system displayed Field labels as follows: >> "view test plan" )', () => {
         cy.get('#lb-collapse').click();
         cy.get('#load-balancer-flavor').click();
-        cy.get('.d-flex > .btn').click(); //Create button
+        cy.contains('Create Flavor').click(); //Create button
+        cy.contains('.card','Load Balancer Flavor Information').within(() => {
+            cy.contains('label', 'Name').should('have.text', 'Name')
+            cy.contains('label', 'Description').should('have.text', 'Description (Optional and will not be displayed on the customer side)')
+            cy.contains('label', 'Flavor ID from Openstack').should('have.text', 'Flavor ID from Openstack')
+            cy.contains('label', 'Max concurrent').should('have.text', 'Max concurrent')
+            cy.contains('label', 'Price').should('have.text', 'Price')
+            cy.contains('label', 'High Availability').should('have.text', 'High Availability')
+            cy.contains('label', 'Published').should('have.text', 'Published')
+        })
 
-        cy.get('.ant-select-selector').click();
-        cy.get(':nth-child(1) > .ant-select-item-option-content').click(); //Data Transfer Price Plan
-        cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('Please input data')
+        cy.contains('.card', 'Network Information').within(() => {
+            cy.contains('label', 'Data Transfer Price Plan').should('have.text', 'Data Transfer Price Plan');
+            cy.contains('label', 'Price International Transfer IN (Price / 1 GB)').should('include.text', 'Price International Transfer IN (Price / 1 GB)');
+            cy.contains('label', 'International Transfer IN').should('include.text', 'International Transfer IN');
+            cy.contains('label', 'Price International Transfer OUT (Price / 1 GB)').should('include.text', 'Price International Transfer OUT (Price / 1 GB)');
+            cy.contains('label', 'International Transfer OUT').should('include.text', 'International Transfer OUT');
+            cy.contains('label', 'Price Domestic Transfer IN (Price / 1 GB)').should('include.text', 'Price Domestic Transfer IN (Price / 1 GB)');
+            cy.contains('label', 'Domestic Transfer IN').should('include.text', 'Domestic Transfer IN');
+            cy.contains('label', 'Price Domestic Transfer OUT (Price / 1 GB)').should('include.text', 'Price Domestic Transfer OUT (Price / 1 GB)');
+            cy.contains('label', 'Domestic Transfer OUT').should('include.text', 'Domestic Transfer OUT');
+            cy.contains('label', 'Private Network Speed').should('have.text', 'Private Network Speed');
 
+        })
 
         cy.wait(700);
 
     })
 
-    it('Validation (Admin did not select field select.  The system display alert messsage “ Please select data” )', () => {
+    it('Validation (Admin did not specify information.  The system display alert messsage “ Please input data” )', () => {
         cy.get('#lb-collapse').click();
         cy.get('#load-balancer-flavor').click();
-        cy.get('.d-flex > .btn').click(); //Create button
-        cy.get('#name').type('Load-Balancer-Flavor1');
-        cy.get('#description').type('Test Load Balance Flavor');
-        cy.get('#flavorId').type('4f2dda44-8797-4317-89de-b4dcdb4c8150');
-        cy.get('#maxConcurrent').type('30');
-        cy.get('#price').type('50');
-        cy.get('#publish').check();
-        cy.get('#internationalTransferIn').type('10');
-        cy.get('#internationalTransferOut').type('10');
-        cy.get('#domesticTransferIn').type('10');
-        cy.get('#domesticTransferOut').type('10');
-        cy.get('#privateNetworkSpeed').type('50');
-        cy.get('[type="submit"]').click();
-        cy.get('.text-danger').contains('Please select data')
+        cy.contains('Create Flavor').click(); //Create button
 
+        cy.get('[type="submit"]').click();
+        cy.get('.text-danger').contains('Please input data')
 
 
         cy.wait(700);
@@ -52,9 +58,7 @@ describe('Create Load Balancer Flavor', () => {
 
         cy.get('#lb-collapse').click();
         cy.get('#load-balancer-flavor').click();
-        cy.get('.d-flex > .btn').click(); //Create button
-        cy.get('.ant-select-selector').click();
-        cy.get(':nth-child(1) > .ant-select-item-option-content').click(); //Data Transfer Price Plan
+        cy.contains('Create Flavor').click(); //Create button
         cy.get('#internationalTransferPriceIn').should('be.disabled').and('have.value', '10');
         cy.get('#internationalTransferPriceOut').should('be.disabled').and('have.value', '10');
         cy.get('#domesticTransferPriceIn').should('be.disabled').and('have.value', '10');
@@ -68,7 +72,7 @@ describe('Create Load Balancer Flavor', () => {
 
         cy.get('#lb-collapse').click();
         cy.get('#load-balancer-flavor').click();
-        cy.get('.d-flex > .btn').click(); //Create button
+        cy.contains('Create Flavor').click(); //Create button
         cy.wait(700);
         cy.get('small').click()
         cy.get('.modal-content')
@@ -85,23 +89,48 @@ describe('Create Load Balancer Flavor', () => {
     it('Action success', () => {
         cy.get('#lb-collapse').click();
         cy.get('#load-balancer-flavor').click();
-        cy.get('.d-flex > .btn').click(); //Create button
+        cy.contains('Create Flavor').click(); //Create button
         cy.get('#name').type('Load-Balancer-Flavor1');
         cy.get('#description').type('Test Load Balance Flavor');
         cy.get('#flavorId').type('4f2dda44-8797-4317-89de-b4dcdb4c8150');
         cy.get('#maxConcurrent').type('30');
         cy.get('#price').type('50');
         cy.get('#publish').check();
-        cy.get('.ant-select-selector').click();
-        cy.get(':nth-child(1) > .ant-select-item-option-content').click(); //Data Transfer Price Plan
-        cy.get('#internationalTransferIn').type('10');
-        cy.get('#internationalTransferOut').type('10');
-        cy.get('#domesticTransferIn').type('10');
-        cy.get('#domesticTransferOut').type('10');
-        cy.get('#privateNetworkSpeed').type('50');
         cy.get('[type="submit"]').click();
 
 
+
+        cy.wait(700);
+
+    })
+
+
+    it('Usibirities (Admin go to Manage Load Balancer Flavor page. The system displayed Field labels as follows: >> "view test plan" )', () => {
+        cy.get('#lb-collapse').click();
+        cy.get('#load-balancer-flavor').click();
+        cy.get('.underline-link').first().click();
+        cy.contains('.card','Load Balancer Flavor Information').within(() => {
+            cy.contains('label', 'Name').should('have.text', 'Name')
+            cy.contains('label', 'Description').should('have.text', 'Description')
+            cy.contains('label', 'Flavor ID from Openstack').should('have.text', 'Flavor ID from Openstack')
+            cy.contains('label', 'Max concurrent').should('have.text', 'Max concurrent')
+            cy.contains('label', 'Price').should('have.text', 'Price')
+            cy.contains('label', 'High Availability').should('have.text', 'High Availability')
+        })
+
+        cy.contains('.card', 'Network Information').within(() => {
+            cy.contains('label', 'Data Transfer Price Plan').should('have.text', 'Data Transfer Price Plan');
+            cy.contains('label', 'Price International Transfer IN (Price / 1 GB)').should('include.text', 'Price International Transfer IN (Price / 1 GB)');
+            cy.contains('label', 'International Transfer IN').should('include.text', 'International Transfer IN');
+            cy.contains('label', 'Price International Transfer OUT (Price / 1 GB)').should('include.text', 'Price International Transfer OUT (Price / 1 GB)');
+            cy.contains('label', 'International Transfer OUT').should('include.text', 'International Transfer OUT');
+            cy.contains('label', 'Price Domestic Transfer IN (Price / 1 GB)').should('include.text', 'Price Domestic Transfer IN (Price / 1 GB)');
+            cy.contains('label', 'Domestic Transfer IN').should('include.text', 'Domestic Transfer IN');
+            cy.contains('label', 'Price Domestic Transfer OUT (Price / 1 GB)').should('include.text', 'Price Domestic Transfer OUT (Price / 1 GB)');
+            cy.contains('label', 'Domestic Transfer OUT').should('include.text', 'Domestic Transfer OUT');
+            cy.contains('label', 'Private Network Speed').should('have.text', 'Private Network Speed (Mbps)');
+
+        })
 
         cy.wait(700);
 
